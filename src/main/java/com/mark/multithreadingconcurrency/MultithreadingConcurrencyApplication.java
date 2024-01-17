6d84294de122a5be7a127a41e5fe4d6ecac5260d
Ethan <yongseok993@gmail.com>
2024-01-17 13:32:29 +0900

    git init

---
+++ src/main/java/com/mark/multithreadingconcurrency/MultithreadingConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.mark.multithreadingconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultithreadingConcurrencyApplication.class, args);
+    }
+
+}
