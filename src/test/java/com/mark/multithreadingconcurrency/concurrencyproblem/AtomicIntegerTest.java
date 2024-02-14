2da48f10bc3b39edcbc157af2458751ca8d9b921
Ethan <yongseok993@gmail.com>
2024-02-14 17:57:44 +0900

    Lock-Free Algorithm - AtomicInteger(Atomic Operation) - AtomicReference<V>

---
+++ src/test/java/com/mark/multithreadingconcurrency/concurrencyproblem/AtomicIntegerTest.java
@@ -0,0 +1,72 @@
+package com.mark.multithreadingconcurrency.concurrencyproblem;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class AtomicIntegerTest {
+    
+    @Test
+    public void atomicIntegerTest() throws Exception {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementThread incrementThread = new IncrementThread(inventoryCounter);
+        DecrementThread decrementThread = new DecrementThread(inventoryCounter);
+
+        incrementThread.start();
+        decrementThread.start();
+
+        incrementThread.join();
+        decrementThread.join();
+
+        log.info("We currently have {} items", inventoryCounter.getItems());
+    }
+
+    public static class DecrementThread extends Thread {
+        private final InventoryCounter inventoryCounter;
+
+        public DecrementThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    public static class IncrementThread extends Thread {
+        private final InventoryCounter inventoryCounter;
+
+        public IncrementThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    public static class InventoryCounter {
+        // synchronized 키워드 없이 원자적 연산 가능
+        private final AtomicInteger items = new AtomicInteger(0);
+
+        public void increment() {
+            items.incrementAndGet();
+        }
+
+        public void decrement() {
+            items.decrementAndGet();
+        }
+
+        public int getItems() {
+            return items.get();
+        }
+    }
+}
---
+++ src/test/java/com/mark/multithreadingconcurrency/concurrencyproblem/AtomicReferenceTest.java
@@ -0,0 +1,160 @@
+package com.mark.multithreadingconcurrency.concurrencyproblem;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.LockSupport;
+
+@Slf4j
+public class AtomicReferenceTest {
+
+    @Test
+    @DisplayName("AtomicReference<T> CAS")
+    public void atomicReferenceTest() {
+        String oldName = "old name";
+        String newName = "new name";
+
+        AtomicReference<String> atomicReference = new AtomicReference<>(oldName);
+
+        // 기대 값과 실제 값이 다르기 때문에 Nothing Changed 출력
+//        atomicReference.set("Unexpected name");
+        if (atomicReference.compareAndSet(oldName, newName)) {
+            log.info("New Value is {}", atomicReference.get());
+        } else {
+            log.info("Nothing Changed");
+        }
+    }
+
+    @Test
+    @DisplayName("Lock-Free Algorithm 이용한 Stack 성능적으로 우수")
+    public void stackTest() throws InterruptedException {
+        LockFreeStack<Integer> stack = new LockFreeStack<>();
+//        StandardStack<Integer> stack = new StandardStack<>();
+        Random random = new Random();
+
+        for (int i = 0; i <100000; i++) {
+            stack.push(random.nextInt());
+        }
+
+        List<Thread> threads = new ArrayList<>();
+        int pushingThreads = 2;
+        int poppingThreads = 2;
+
+        for (int i = 0; i < pushingThreads; i++) {
+            Thread thread = new Thread(() -> {
+                while (true) {
+                    stack.push(random.nextInt());
+                }
+            });
+
+            thread.setDaemon(true);
+            threads.add(thread);
+        }
+
+        for (int i = 0; i <poppingThreads; i++) {
+            Thread thread = new Thread(() -> {
+                while (true) {
+                    stack.pop();
+                }
+            });
+
+            thread.setDaemon(true);
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        Thread.sleep(10000);
+
+        log.info("{} operations were performed in 10 seconds", stack.getCounter());
+    }
+
+    public static class LockFreeStack<T> {
+        private final AtomicReference<StackNode<T>> head = new AtomicReference<>();
+        private final AtomicInteger counter = new AtomicInteger(0);
+
+        public void push(T value) {
+            StackNode<T> newHeadNode = new StackNode<>(value);
+
+            while (true) {
+                StackNode<T> currentHeadNode = head.get();
+                newHeadNode.next = currentHeadNode;
+
+                if (head.compareAndSet(currentHeadNode, newHeadNode)) {
+                    break;
+                } else {
+                    LockSupport.parkNanos(1);
+                }
+            }
+
+            counter.incrementAndGet();
+        }
+
+        public T pop() {
+            StackNode<T> currentHeadNode = head.get();
+            StackNode<T> newHeadNode;
+
+            while (currentHeadNode != null) {
+                newHeadNode = currentHeadNode.next;
+                if (head.compareAndSet(currentHeadNode, newHeadNode)) {
+                    break;
+                } else {
+                    LockSupport.parkNanos(1);
+                    currentHeadNode = head.get();
+                }
+            }
+
+            counter.incrementAndGet();
+            return currentHeadNode != null ? currentHeadNode.value : null;
+        }
+
+        public int getCounter() {
+            return counter.get();
+        }
+    }
+
+    public static class StandardStack<T> {
+        private StackNode<T> head;
+
+        @Getter
+        private int counter = 0;
+
+        public synchronized void push(T value) {
+            StackNode<T> newHead = new StackNode<>(value);
+            newHead.next = head;
+            head = newHead;
+            counter++;
+        }
+
+        public synchronized T pop() {
+            if (head == null) {
+                counter++;
+                return null;
+            }
+
+            T value = head.value;
+            head = head.next;
+            counter++;
+            return value;
+        }
+    }
+
+    public static class StackNode<T> {
+        public T value;
+        public StackNode<T> next;
+
+        public StackNode(T value) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
