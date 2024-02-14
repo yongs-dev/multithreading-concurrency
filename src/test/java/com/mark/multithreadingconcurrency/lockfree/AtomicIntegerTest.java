64f031085827594de4317a8fed65df9da44c3d05
Ethan <yongseok993@gmail.com>
2024-02-14 18:01:15 +0900

    Lock-Free Algorithm - packaging

--- src/test/java/com/mark/multithreadingconcurrency/lockfree/AtomicIntegerTest.java
+++ src/test/java/com/mark/multithreadingconcurrency/lockfree/AtomicIntegerTest.java
@@ -1,4 +1,4 @@
-package com.mark.multithreadingconcurrency.concurrencyproblem;
+package com.mark.multithreadingconcurrency.lockfree;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
--- src/test/java/com/mark/multithreadingconcurrency/lockfree/AtomicReferenceTest.java
+++ src/test/java/com/mark/multithreadingconcurrency/lockfree/AtomicReferenceTest.java
@@ -1,4 +1,4 @@
-package com.mark.multithreadingconcurrency.concurrencyproblem;
+package com.mark.multithreadingconcurrency.lockfree;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
